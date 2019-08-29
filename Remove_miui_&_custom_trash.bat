eadfeb95afd156e95a208cf7c1439bcaf73b47b8
Redian23 <vasya.degski@gmail.com>
2019-08-29 19:50:19 +0300

    Add bash script

---
+++ Remove_miui_&_custom_trash.bat
@@ -0,0 +1,34 @@
+@echo off
+:STARTS
+CLS
+
+adb devices
+ECHO. Remover Google trash from MIUI
+ECHO. by Restorm
+ECHO. Remove Google Docs
+adb shell pm uninstall -k --user 0 com.google.android.apps.docs
+ECHO. Remove Google Photo
+adb shell pm uninstall -k --user 0 com.google.android.apps.photos
+ECHO. Remove Google Music
+adb shell pm uninstall -k --user 0 com.google.android.music
+ECHO. Remove Google Video
+adb shell pm uninstall -k --user 0 com.google.android.videos
+ECHO. Remove Google Play Games
+adb shell pm uninstall -k --user 0 com.google.android.play.games
+ECHO. Remove Due
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+ECHO. Remove Google Search
+adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
+ECHO. Remove Google Map
+adb shell pm uninstall -k --user 0 com.google.android.apps.maps
+ECHO. Remove Google Mail
+adb shell pm uninstall -k --user 0 com.google.android.gm
+
+ECHO. 
+ECHO. IT'S ALL
+rem Чтобы удалить другую программу,впишите ее адрес "com.сяоми.прога.какае-то" 
+rem Найти этот адрес можно в описании к программе 
+rem Также можете закоментировть c помощью "rem" нужные программы
+rem и спасти их от удаления
+:CHO
+pause
--- Remove_miui_and_custom_trash.sh
+++ Remove_miui_and_custom_trash.sh
@@ -1,34 +1,24 @@
-@echo off
-:STARTS
-CLS
+clear
+echo -e "\e[32mRemover Google trash from MIUI & Custom ROMs\e[0m"
+echo -e "\e[32mby Restorm\e[0m"
 
 adb devices
-ECHO. Remover Google trash from MIUI
-ECHO. by Restorm
-ECHO. Remove Google Docs
-adb shell pm uninstall -k --user 0 com.google.android.apps.docs
-ECHO. Remove Google Photo
-adb shell pm uninstall -k --user 0 com.google.android.apps.photos
-ECHO. Remove Google Music
-adb shell pm uninstall -k --user 0 com.google.android.music
-ECHO. Remove Google Video
-adb shell pm uninstall -k --user 0 com.google.android.videos
-ECHO. Remove Google Play Games
-adb shell pm uninstall -k --user 0 com.google.android.play.games
-ECHO. Remove Due
-adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
-ECHO. Remove Google Search
-adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
-ECHO. Remove Google Map
-adb shell pm uninstall -k --user 0 com.google.android.apps.maps
-ECHO. Remove Google Mail
-adb shell pm uninstall -k --user 0 com.google.android.gm
 
-ECHO. 
-ECHO. IT'S ALL
-rem Чтобы удалить другую программу,впишите ее адрес "com.сяоми.прога.какае-то" 
-rem Найти этот адрес можно в описании к программе 
-rem Также можете закоментировть c помощью "rem" нужные программы
-rem и спасти их от удаления
-:CHO
-pause
+  echo Remove Google Docs
+adb shell pm uninstall -k --user 0 com.google.android.apps.docs
+  echo Remove Google Photo
+adb shell pm uninstall -k --user 0 com.google.android.apps.photos
+  echo Remove Google Musicecho
+adb shell pm uninstall -k --user 0 com.google.android.music
+  echo Remove Google Video
+adb shell pm uninstall -k --user 0 com.google.android.videos
+  echo Remove Google Play Games
+adb shell pm uninstall -k --user 0 com.google.android.play.games
+  echo Remove Due
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+  echo Remove Google Search
+adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
+  echo Remove Google Map
+adb shell pm uninstall -k --user 0 com.google.android.apps.maps
+  echo Remove Google Mail
+adb shell pm uninstall -k --user 0 com.google.android.gm
