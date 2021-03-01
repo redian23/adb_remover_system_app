94e3b6249bbb331e8453f089dbc26df4ca1d4373
Redian23 <vasya.degski@gmail.com>
2021-03-01 18:16:33 +0300

    update lx script

--- linux_remover.sh
+++ linux_remover.sh
@@ -1,33 +1,90 @@
+#!/bin/bash
 clear
 echo -e "\e[32mRemover Google trash from MIUI & Custom ROMs\e[0m"
 echo -e "\e[32mby Restorm\e[0m"
 
 adb devices
 
-  echo Remove Google Docs
-adb shell pm uninstall -k --user 0 com.google.android.apps.docs
-  echo Remove Google Photo
-adb shell pm uninstall -k --user 0 com.google.android.apps.photos
-  echo Remove Google Music
-adb shell pm uninstall -k --user 0 com.google.android.music
-  echo Remove Google Video
-adb shell pm uninstall -k --user 0 com.google.android.videos
-  echo Remove Google Play Games
-adb shell pm uninstall -k --user 0 com.google.android.play.games
-  echo Remove Due
-adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
-  echo Remove Google Search
-adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
-  echo Remove Google Map
-adb shell pm uninstall -k --user 0 com.google.android.apps.maps
-::  echo Remove Google Mail
-::adb shell pm uninstall -k --user 0 com.google.android.gm
-  echo Remove YouTube
-adb shell pm uninstall -k --user 0 com.google.android.youtube
-  echo Remove Mi Drop
-adb shell pm uninstall -k --user 0 com.xiaomi.midrop
+echo "List Google app:"
+echo "1.Docs, 2.Photo, 3.Music, 4.Video, 5.Games,
+6.Due, 7.G.Search, 8.Map, 9.Mail, 10.YouTube
+"
+echo "Xiaomi apps:"
+echo "11. Mi Drop
+"   
+echo "12+ Exit"
 
-:: Чтобы удалить другую программу,впишите ее адрес "com.сяоми.прога"
-:: Найти этот адрес можно в описании приложения.
-:: Вы всегда можете закоментировать нужное вам приложение добавив "::"
-:: в начале строки перед командой.
+while :
+do
+  read INPUT_STRING
+  case $INPUT_STRING in
+    1)
+      adb shell pm uninstall -k --user 0 com.google.android.apps.docs
+      echo Google Docs removed
+      continue
+      ;;
+    2) 
+      adb shell pm uninstall -k --user 0 com.google.android.apps.photos
+      echo Google Photo removed
+      continue
+      ;;
+    3)    
+      adb shell pm uninstall -k --user 0 com.google.android.music
+      echo Google Music removed
+      continue
+      ;;
+    4)    
+      adb shell pm uninstall -k --user 0 com.google.android.videos
+      echo Google Video removed
+      continue
+      ;;
+    5)  
+      adb shell pm uninstall -k --user 0 com.google.android.play.games  
+      echo Google Play Games removed
+      continue
+      ;;  
+    6)
+      adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+      echo Due removed
+      continue
+      ;;      
+    7)
+      adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
+      echo Google Search removed
+      continue
+      ;;          
+    8)
+      adb shell pm uninstall -k --user 0 com.google.android.apps.maps
+      echo Google Map removed
+      continue
+      ;;          
+    9)  
+      adb shell pm uninstall -k --user 0 com.google.android.gm
+      echo Google Mail removed
+      continue
+      ;;          
+    10)
+      adb shell pm uninstall -k --user 0 com.google.android.youtube
+      echo YouTube removed
+      continue
+      ;;  
+    11)
+      adb shell pm uninstall -k --user 0 com.xiaomi.midrop 
+      echo Mi Drop removed
+      continue
+      ;;  
+    *)
+      echo "You are select EXIT"
+      break
+      ;;
+  esac
+done
+
+echo Done. 
+echo All useless app was beed removed.
+
+# Чтобы удалить другую программу,впишите ее адрес "com.сяоми.прога"
+# Например adb shell pm uninstall -k --user 0 com.exemple.app
+# Найти этот адрес можно в описании приложения.
+# Вы всегда можете закоментировать нужное вам приложение добавив "#"
+# в начале строки перед командой.
--- win_remover.bat
+++ win_remover.bat
@@ -1,4 +1,4 @@
-@echo off
+@ECHO. off
 :STARTS
 CLS
 
@@ -7,28 +7,80 @@ adb devices
 ECHO. "Remover Google trash from MIUI and Custom ROMs"
 ECHO. by Restorm
 
-ECHO. Remove Google Docs
-adb shell pm uninstall -k --user 0 com.google.android.apps.docs
-ECHO. Remove Google Photo
-adb shell pm uninstall -k --user 0 com.google.android.apps.photos
-ECHO. Remove Google Music
-adb shell pm uninstall -k --user 0 com.google.android.music
-ECHO. Remove Google Video
-adb shell pm uninstall -k --user 0 com.google.android.videos
-ECHO. Remove Google Play Games
-adb shell pm uninstall -k --user 0 com.google.android.play.games
-ECHO. Remove Due
-adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
-ECHO. Remove Google Search
-adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
-ECHO. Remove Google Map
-adb shell pm uninstall -k --user 0 com.google.android.apps.maps
-ECHO. Remove Google Mail
-adb shell pm uninstall -k --user 0 com.google.android.gm
-ECHO. Remove YouTube
-adb shell pm uninstall -k --user 0 com.google.android.youtube
-ECHO. Remove Mi Drop
-adb shell pm uninstall -k --user 0 com.xiaomi.midrop
+ECHO. "List Google app:"
+ECHO. "1.Docs, 2.Photo, 3.Music, 4.Video, 5.Games"
+ECHO. "6.Due, 7.G.Search, 8.Map, 9.Mail, 10.YouTube "
+
+ECHO. "Xiaomi apps:"
+ECHO. "11. Mi Drop "   
+
+ECHO. "12+ Exit"
+
+while :
+do
+  read INPUT_STRING
+  case $INPUT_STRING in
+    1)
+      adb shell pm uninstall -k --user 0 com.google.android.apps.docs
+      ECHO. Google Docs removed
+      continue
+      ;;
+    2) 
+      adb shell pm uninstall -k --user 0 com.google.android.apps.photos
+      ECHO. Google Photo removed
+      continue
+      ;;
+    3)    
+      adb shell pm uninstall -k --user 0 com.google.android.music
+      ECHO. Google Music removed
+      continue
+      ;;
+    4)    
+      adb shell pm uninstall -k --user 0 com.google.android.videos
+      ECHO. Google Video removed
+      continue
+      ;;
+    5)  
+      adb shell pm uninstall -k --user 0 com.google.android.play.games  
+      ECHO. Google Play Games removed
+      continue
+      ;;  
+    6)
+      adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+      ECHO. Due removed
+      continue
+      ;;      
+    7)
+      adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
+      ECHO. Google Search removed
+      continue
+      ;;          
+    8)
+      adb shell pm uninstall -k --user 0 com.google.android.apps.maps
+      ECHO. Google Map removed
+      continue
+      ;;          
+    9)  
+      adb shell pm uninstall -k --user 0 com.google.android.gm
+      ECHO. Google Mail removed
+      continue
+      ;;          
+    10)
+      adb shell pm uninstall -k --user 0 com.google.android.youtube
+      ECHO. YouTube removed
+      continue
+      ;;  
+    11)
+      adb shell pm uninstall -k --user 0 com.xiaomi.midrop 
+      ECHO. Mi Drop removed
+      continue
+      ;;  
+    *)
+      ECHO. "You are select EXIT"
+      break
+      ;;
+  esac
+done
 
 ECHO.
 ECHO. IT'S ALL
