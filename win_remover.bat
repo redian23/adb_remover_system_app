fd531826d2b7f90fb206915f85479b5ed16d529f
Redian23 <vasya.degski@gmail.com>
2021-03-01 19:05:40 +0300

    fix win script

--- win_remover.bat
+++ win_remover.bat
@@ -1,92 +1,48 @@
-@ECHO. off
-:STARTS
-CLS
+@ECHO OFF
 
-adb devices
 
-ECHO. "Remover Google trash from MIUI and Custom ROMs"
-ECHO. by Restorm
+ECHO Remover Google trash from MIUI and Custom ROMs
+ECHO by Restorm
 
-ECHO. "List Google app:"
-ECHO. "1.Docs, 2.Photo, 3.Music, 4.Video, 5.Games"
-ECHO. "6.Due, 7.G.Search, 8.Map, 9.Mail, 10.YouTube "
+ECHO List Google app:
+ECHO 1.Docs, 2.Photo, 3.Music, 4.Video, 5.Games 6.Due, 7.G.Search, 8.Map, 9.Mail, 10.YouTube
 
-ECHO. "Xiaomi apps:"
-ECHO. "11. Mi Drop "   
+ECHO Xiaomi apps:
+ECHO 11. Mi Drop   
 
-ECHO. "12+ Exit"
+ECHO 12+ Exit
+:START
+SET /P UserInput=Please Enter a Number:
+IF %UserInput% EQU 0 GOTO E_INVALIDINPUT
 
-while :
-do
-  read INPUT_STRING
-  case $INPUT_STRING in
-    1)
-      adb shell pm uninstall -k --user 0 com.google.android.apps.docs
-      ECHO. Google Docs removed
-      continue
-      ;;
-    2) 
-      adb shell pm uninstall -k --user 0 com.google.android.apps.photos
-      ECHO. Google Photo removed
-      continue
-      ;;
-    3)    
-      adb shell pm uninstall -k --user 0 com.google.android.music
-      ECHO. Google Music removed
-      continue
-      ;;
-    4)    
-      adb shell pm uninstall -k --user 0 com.google.android.videos
-      ECHO. Google Video removed
-      continue
-      ;;
-    5)  
-      adb shell pm uninstall -k --user 0 com.google.android.play.games  
-      ECHO. Google Play Games removed
-      continue
-      ;;  
-    6)
-      adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
-      ECHO. Due removed
-      continue
-      ;;      
-    7)
-      adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
-      ECHO. Google Search removed
-      continue
-      ;;          
-    8)
-      adb shell pm uninstall -k --user 0 com.google.android.apps.maps
-      ECHO. Google Map removed
-      continue
-      ;;          
-    9)  
-      adb shell pm uninstall -k --user 0 com.google.android.gm
-      ECHO. Google Mail removed
-      continue
-      ;;          
-    10)
-      adb shell pm uninstall -k --user 0 com.google.android.youtube
-      ECHO. YouTube removed
-      continue
-      ;;  
-    11)
-      adb shell pm uninstall -k --user 0 com.xiaomi.midrop 
-      ECHO. Mi Drop removed
-      continue
-      ;;  
-    *)
-      ECHO. "You are select EXIT"
-      break
-      ;;
-  esac
-done
+SET /A UserInputVal="%UserInput%"*1
+IF %UserInputVal%==1 (adb shell pm uninstall -k --user 0 com.google.android.apps.docs  
+ECHO. Google Docs removed)
+IF %UserInputVal%==2 (adb shell pm uninstall -k --user 0 com.google.android.apps.photos
+ECHO. Google Photo removed)
+IF %UserInputVal%==3 (adb shell pm uninstall -k --user 0 com.google.android.music
+ECHO. Google Music removed)
+IF %UserInputVal%==4 (adb shell pm uninstall -k --user 0 com.google.android.videos
+ECHO. Google Video removed)
+IF %UserInputVal%==5 (adb shell pm uninstall -k --user 0 com.google.android.play.games  
+ECHO. Google Play Games removed)
+IF %UserInputVal%==6 (adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+ECHO. Due removed)
+IF %UserInputVal%==7 (adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
+ECHO. Google Search removed)
+IF %UserInputVal%==8 (adb shell pm uninstall -k --user 0 com.google.android.apps.maps
+ECHO. Google Map removed)
+IF %UserInputVal%==9 (adb shell pm uninstall -k --user 0 com.google.android.gm
+ECHO. Google Mail removed)
+IF %UserInputVal%==10 (adb shell pm uninstall -k --user 0 com.google.android.youtube
+ECHO. YouTube removed)
+IF %UserInputVal%==11 (adb shell pm uninstall -k --user 0 com.xiaomi.midrop 
+ECHO. Mi Drop removed)
+IF %UserInputVal% GTR 11 ( GOTO EXIT)
+GOTO START
 
-ECHO.
-ECHO. IT'S ALL
-rem ����� ������� ������ ���������,������� �� ����� "com.����.�����"
-rem ����� ���� ����� ����� � �������� � ���������
-rem ����� ������ �������������� c ������� "rem" ������ ���������
-rem � ������ �� �� ��������
-:CHO
+:E_INVALIDINPUT
+ECHO Invalid user input
+
+:EXIT
 pause
